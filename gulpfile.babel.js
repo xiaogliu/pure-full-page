1c15c09e914d36ce5e3acfe74900fbb08fd01798
liuxiaoguang <vincexgliu@gmail.com>
2018-04-15 02:38:12 +0800

    gulp change

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,18 @@
 import babel from 'gulp-babel';
 import gulp from 'gulp';
-import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
 
-gulp.task('babel', () => {
+const srcFiles = [
+  './src/no_module/js/components/utils.js',
+  './src/no_module/js/components/pureFullPage.js',
+];
+
+gulp.task('transferJS', () => {
   gulp
-    .src('./src/no_module/js/components/pureFullPage.js')
+    .src(srcFiles)
+    .pipe(concat('pureFullPage.min.js'))
     .pipe(babel())
     .pipe(uglify())
-    .pipe(rename('pureFullPage.min.js'))
     .pipe(gulp.dest('./dist'));
-  console.log(111);
 });
