bd66f3824776d0c2b47c1f788abb666af0e15356
liuxiaoguang <vincexgliu@gmail.com>
2018-04-17 11:53:24 +0800

    调整目录结构

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,17 +2,35 @@ import babel from 'gulp-babel';
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
 import concat from 'gulp-concat';
+import cleanCSS from 'gulp-clean-css';
+import sass from 'gulp-sass';
+import rename from 'gulp-rename';
 
-const srcFiles = [
-  './src/no_module/js/components/utils.js',
-  './src/no_module/js/components/pureFullPage.js',
-];
+const srcJSFiles = ['./src/js/utils.js', './src/js/pureFullPage.js'];
 
 gulp.task('transferJS', () => {
   gulp
-    .src(srcFiles)
+    .src(srcJSFiles)
     .pipe(concat('pureFullPage.min.js'))
     .pipe(babel())
     .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('transferCSS', () => {
+  gulp
+    .src('./src/css/pureFullPage.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({ compatibility: '*' }))
+    .pipe(rename('pureFullPage.min.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+// 监控
+gulp.task('watch', function() {
+  gulp.watch(srcJSFiles, ['transferJS']);
+  gulp.watch('./src/css/pureFullPage.scss', ['transferCSS']);
+});
+
+// 打包发布
+gulp.task('build', ['transferJS', 'transferCSS']);
--- src/no_module/js/index.js
+++
@@ -1,1 +0,0 @@
-new PureFullPage().init();
